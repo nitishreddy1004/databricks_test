b116053bd8c8f35f6ad07e54a98742b31183023d
nitishreddy1004@gmail.com <nitishreddy1004@gmail.com>
2023-08-21 18:04:07 +0000

    initial commit

---
+++ f1/n1.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC
+# MAGIC create table st(roll int,name varchar(23));
+# MAGIC select * from st;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from st;
